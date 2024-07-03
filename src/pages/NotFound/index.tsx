e59fa0959e0444f96c1cb4e752d9012491dd5fc6
André Lucas <andrelucca99@gmail.com>
2024-07-03 09:26:46 -0300

    style: add style in not page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,15 @@
+import { Link } from 'react-router-dom';
+import fundo from '../../assets/capa.jpg';
+
 function NotFound() {
   return (
-    <h1>Pagina não encontrada</h1>
+    <div
+      style={ { backgroundImage: `url(${fundo})` } }
+      className="bg-cover h-screen flex flex-col justify-center gap-5 items-center text-white"
+    >
+      <h1 className="text-3xl font-extrabold">Pagina não encontrada</h1>
+      <Link to="/" className="hover:cursor-pointer hover:bg-blue-600 hover:rounded-md hover:px-5 hover:text-white">Voltar</Link>
+    </div>
   );
 }
 
