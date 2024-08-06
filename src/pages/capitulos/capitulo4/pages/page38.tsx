4efc5ba0cfc6f46b3068650d9117a06cb2ed8db0
André Lucas <andrelucca99@gmail.com>
2024-08-06 12:56:15 -0300

    refactor: retirar texto estatico para texto dinanmico com o componente modal

--- src/pages/capitulos/capitulo4/pages/page38.tsx
+++ src/pages/capitulos/capitulo4/pages/page38.tsx
@@ -1,13 +1,16 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import BackgroundPage from '../../../../components/BackgroundPage';
 import Sheet from '../../../../components/Sheet';
 import Paragraph from '../../../../components/Paragraph';
 import Button from '../../../../components/Button';
 import PagesContext from '../../../../context/PagesContext';
+import Modal from '../../../../components/Modal';
 
 function Page38() {
   const { paginas } = useContext(PagesContext);
   const { left, right } = paginas.page36;
+
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
     <BackgroundPage>
@@ -19,7 +22,44 @@ function Page38() {
                 <p key={ index }>{page}</p>
               ))
             }
+            <div className="p-10 flex justify-center w-full">
+              <button
+                className="border border-neutral-300 rounded-lg py-1.5 px-10 bg-blue-500 hover:bg-blue-600 text-white"
+                onClick={ () => setOpen(true) }
+              >
+                Ler Carta
+              </button>
+              <Modal open={ open } onClose={ () => setOpen(false) }>
+                <div className="flex flex-col gap-4">
+                  <h1>ESCOLA DE MAGIA E BRUXARIA DE HOGWARTS</h1>
+                  <p className="text-center text-sm">
+                    Diretor: Alvo Dumbledore
+                    <br />
+                    (Ordem de Merlim, Primeira Classe, Grande Feiticeiro, Bruxo Chefe, Cacique Supremo,
+                    Confederação Internacional de Bruxos)
+                  </p>
+                  <p>
+                    <strong>Prezado Sr. Potter</strong>
+                    ,
+                    <br />
+                    Temos o prazer de informar que V. Sa. tem uma vaga na Escola de Magia e Bruxaria de
+                    Hogwarts. Estamos anexando uma lista dos livros e equipamentos necessários.
+                    O ano letivo começa em 1ode setembro. Aguardamos sua coruja até 31 de julho, no mais
+                    tardar.
+                    <br />
+                    <br />
+                    Atenciosamente,
+                  </p>
+                  <p>
+                    Minerva McGonagall
+                    <br />
+                    Diretora Substituta
+                  </p>
+                </div>
+              </Modal>
+            </div>
           </Paragraph>
+
         }
         right={
           <Paragraph>
