4b5b430df2c44eb807a2269abca34e8e13c62475
André Lucas <andrelucca99@gmail.com>
2025-11-02 09:53:40 -0300

    style: ajuste no estilo do componente de capa do capitulo

--- src/components/CapaCapitulo/index.tsx
+++ src/components/CapaCapitulo/index.tsx
@@ -6,8 +6,13 @@ type Props = {
 const CapaCapitulo: React.FC<Props> = ({ capa }) => {
   return (
     <div
-      style={ { backgroundImage: `url(${capa})` } }
-      className="bg-cover mobile:h-96 mobile:my-10 tablet:h-full tablet:my-0 w-full"
+      style={{
+        backgroundImage: `url(${capa})`,
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+      className="w-full h-full mobile:h-96 mobile:my-10 tablet:h-full tablet:my-0"
     />
   );
 };
