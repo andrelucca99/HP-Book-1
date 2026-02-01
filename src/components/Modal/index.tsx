4cfc4b0d1ef15be3eb6474c03759184fa976b913
André Lucas <andrelucca99@gmail.com>
2026-02-01 07:58:44 -0300

    fix: modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -7,16 +7,16 @@ type ModalProps = {
 const Modal: React.FC<ModalProps> = ({ open, onClose, children }) => {
   return (
     <div
-      className={ `fixed inset-0 flex justify-center items-center transition-colors ${open ? 'visible bg-black/20' : 'invisible'}` }
-      onClick={ onClose }
+      className={`fixed inset-0 flex justify-center items-center transition-colors ${open ? 'visible bg-black/20' : 'invisible'}`}
+      onClick={onClose}
     >
       <div
-        className={ `bg-white rounded-lg shadow p-6 transition-all max-w-md ${open ? 'scale-100 opacity-100 w-4/5 h-4/5 overflow-auto' : 'scale-110 opacity-0'} snap-y` }
-        onClick={ (e) => e.stopPropagation() }
+        className={`bg-white rounded-lg shadow p-6 transition-all max-w-md ${open ? 'scale-100 opacity-100 w-4/5 h-4/5 overflow-auto' : 'scale-110 opacity-0'} snap-y`}
+        onClick={(e) => e.stopPropagation()}
       >
         <button
           className="absolute top-2 right-2 py-1 px-2 rounded-md text-gray-400 hover:text-gray-600"
-          onClick={ (onClose) }
+          onClick={(onClose)}
         >
           X
         </button>
